7d7318be04e63172ce5c21effab56384cd1257ec
joss12 <drh374@mail.com>
2026-03-18 10:14:21 -0400

    photo sized updated again

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
                         <img
                             src="/photo/KEM.jpg"
                             alt="BornToShine"
-                            style={{ width: '250px', height: '250px', borderRadius: '60%' }}
+                            style={{ width: '270px', height: '270px', borderRadius: '60%' }}
                             className="rounded-full object-cover border-2 border-cyan-500/30"
                         />
                     </div>
