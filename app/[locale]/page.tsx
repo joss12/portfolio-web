b9806b906ca5b6930e4370cb083d717741ff83b5
joss12 <drh374@mail.com>
2026-03-18 09:52:17 -0400

    fix: photo size updated again

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
                         <img
                             src="/photo/resume.jpg"
                             alt="BornToShine"
-                            style={{ width: '190px', height: '190px' }}
+                            style={{ width: '200px', height: '200px' }}
                             className="rounded-full object-cover border-2 border-cyan-500/30"
                         />
                     </div>
